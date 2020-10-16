b924bd1c7369cb4a22a9bbc51fb9cccc09681911
Samir Banik <samir.banik@niser.ac.in>
2020-10-16 09:41:23 +0530

    Added G4ParticleGun and integrated CRY into the setup

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -27,37 +27,164 @@
 //
 
 #include "PrimaryGeneratorAction.hh"
-
+#include "PrimaryGeneratorMessenger.hh"
+#include <iomanip>
 #include "G4Event.hh"
 #include "G4GeneralParticleSource.hh"
 #include "G4ParticleTable.hh"
 #include "G4ParticleDefinition.hh"
 #include "globals.hh"
-
+#include "CRYSetup.h"
+#include "CRYGenerator.h"
+#include "CRYParticle.h"
+#include "CRYUtils.h"
+#include "G4SystemOfUnits.hh"
 //#include "radsource.h"
 //#include "cpp_api.h"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-PrimaryGeneratorAction::PrimaryGeneratorAction()
+PrimaryGeneratorAction::PrimaryGeneratorAction(const char *inputfile)
 : G4VUserPrimaryGeneratorAction(),
-fParticleGun(0)
+  fParticleGun(0),
+  fParticleSource(0),
+  SourceZPosition(0)
 {
-    fParticleGun = new G4GeneralParticleSource();
+  fParticleGun    = new G4ParticleGun();
+  fParticleSource = new G4GeneralParticleSource();
+  std::ifstream inputFile;
+  inputFile.open(inputfile,std::ios::in);
+  char buffer[1000];
+
+  if (inputFile.fail()) {
+    if( *inputfile !=0)  //....only complain if a filename was given
+      G4cout << "PrimaryGeneratorAction: Failed to open CRY input file= " << inputfile << G4endl;
+    InputState=-1;
+  }else{
+    std::string setupString("");
+    while ( !inputFile.getline(buffer,1000).eof()) {
+      setupString.append(buffer);
+      setupString.append(" ");
+    }
+
+    //CRYSetup *setup=new CRYSetup(setupString,"cosmic_data");
+    //CRYSetup *setup=new CRYSetup(setupString,"/home/samir/cry_v1.7/data/cosmics_0.data");
+    CRYSetup *setup=new CRYSetup(setupString,"/home/samir/cry_v1.7/data");
+
+    gen = new CRYGenerator(setup);
+
+    // set random number generator
+    // RNGWrapper<CLHEP::HepRandomEngine>::set(CLHEP::HepRandom::getTheEngine(),&CLHEP::HepRandomEngine::flat);
+    // setup->setRandomFunction(RNGWrapper<CLHEP::HepRandomEngine>::rng);
+    InputState=0;
+  }
+  // create a vector to store the CRY particle properties
+  vect=new std::vector<CRYParticle*>;
+
+  // Create the table containing all particle names
+  particleTable = G4ParticleTable::GetParticleTable();
+  fMessenger = new PrimaryGeneratorMessenger(this);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
+void PrimaryGeneratorAction::UpdateCRY(std::string* MessInput)
+{
+  CRYSetup *setup=new CRYSetup(*MessInput,"/home/samir/cry_v1.7/data");
+  gen = new CRYGenerator(setup);
+  // set random number generator
+  // RNGWrapper<CLHEP::HepRandomEngine>::set(CLHEP::HepRandom::getTheEngine(),&CLHEP::HepRandomEngine::flat);
+  // setup->setRandomFunction(RNGWrapper<CLHEP::HepRandomEngine>::rng);
+  InputState=0;
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PrimaryGeneratorAction::UpdateCRYFile(G4String newValue)
+{
+  G4cout<<"Input file is=> "<<newValue<<G4endl;
+  // Read the cry input file
+  std::ifstream inputFile;
+  inputFile.open(newValue,std::ios::in);
+  char buffer[1000];
+
+  if (inputFile.fail()) {
+    G4cout << "Failed to open input file " << newValue << G4endl;
+    G4cout << "Make sure to define the cry library on the command line" << G4endl;
+    InputState=-1;
+  }else{
+    std::string setupString("");
+    while ( !inputFile.getline(buffer,1000).eof()) {
+      setupString.append(buffer);
+      setupString.append(" ");
+    }
+
+    CRYSetup *setup=new CRYSetup(setupString,"/home/samir/cry_v1.7/data");
+    gen = new CRYGenerator(setup);
+
+    // set random number generator
+    // RNGWrapper<CLHEP::HepRandomEngine>::set(CLHEP::HepRandom::getTheEngine(),&CLHEP::HepRandomEngine::flat);
+    // setup->setRandomFunction(RNGWrapper<CLHEP::HepRandomEngine>::rng);
+    InputState=0;
+  }
+}
+void PrimaryGeneratorAction::SetSourceZPosition(G4double dist)
+{
+  SourceZPosition = dist;
+}
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 PrimaryGeneratorAction::~PrimaryGeneratorAction()
 {
-    delete fParticleGun;
+  delete fParticleGun;
+  delete fParticleSource;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 {
+  
+if (InputState != 0) {
+    G4String* str = new G4String("CRY library was not successfully initialized");
+    //G4Exception(*str);
+    G4Exception("CryPrimaryGeneratorAction", "1",
+                RunMustBeAborted, *str);
+  }
+  G4String particleName;
+  vect->clear();
+  gen->genEvent(vect);
+
+  //....debug output
+  // G4cout << "\nEvent=" << anEvent->GetEventID() << " "
+  //        << "CRY generated nparticles=" << vect->size()
+  //        << G4endl;
+  for ( unsigned j=0; j<vect->size(); j++) {
+    particleName=CRYUtils::partName((*vect)[j]->id());
+
+    //if( particleName == "electron" ) continue;
+
+
+
+    //....debug output  
+    // G4cout << "  "          << particleName << " "
+    // 	   << "charge="      << (*vect)[j]->charge() << " "
+    // 	   << "energy (MeV)=" << (*vect)[j]->ke()*MeV << " "
+    // 	   << "pos (m)"
+    // 	   << G4ThreeVector((*vect)[j]->x(), (*vect)[j]->y(), (*vect)[j]->z())
+    // 	   << " " << "direction cosines "
+    // 	   << G4ThreeVector((*vect)[j]->u(), (*vect)[j]->v(), (*vect)[j]->w())
+    // 	   << " " << G4endl;
+    
+    fParticleGun->SetParticleDefinition(particleTable->FindParticle((*vect)[j]->PDGid()));
+    fParticleGun->SetParticleEnergy((*vect)[j]->ke()*MeV);
+    fParticleGun->SetParticlePosition(G4ThreeVector((*vect)[j]->x()*m, (*vect)[j]->y()*m, (*vect)[j]->z()*m + SourceZPosition));
+    fParticleGun->SetParticleMomentumDirection(G4ThreeVector((*vect)[j]->u(), (*vect)[j]->v(), (*vect)[j]->w()));
+    fParticleGun->SetParticleTime((*vect)[j]->t());
     fParticleGun->GeneratePrimaryVertex(anEvent);
+    delete (*vect)[j];
+  }
+  //fParticleSource->GeneratePrimaryVertex(anEvent);
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo....
