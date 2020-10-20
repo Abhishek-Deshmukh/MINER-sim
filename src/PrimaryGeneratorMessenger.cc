d868f65b0649c62e48a9a8deeca5a64a5205ab6f
Samir Banik <samir.banik@niser.ac.in>
2020-10-20 20:08:50 +0530

    added Messenger class for PrimaryGeneratorAction

---
+++ src/PrimaryGeneratorMessenger.cc
@@ -0,0 +1,87 @@
+//
+// $Id: PrimaryGeneratorMessenger.cc,v 1.8 2002/12/16 16:37:27 maire Exp $
+// GEANT4 tag $Name: geant4-07-00-patch-01 $
+//
+// 
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "PrimaryGeneratorMessenger.hh"
+
+#include "PrimaryGeneratorAction.hh"
+#include "G4UIdirectory.hh"
+#include "G4UIcmdWithAString.hh"
+#include "G4UIcmdWithoutParameter.hh"
+#include "G4UIcmdWithADoubleAndUnit.hh"
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PrimaryGeneratorMessenger::PrimaryGeneratorMessenger(PrimaryGeneratorAction* Gun)
+  :Action(Gun)
+{
+  CRYDir = new G4UIdirectory("/CRY/");
+  CRYDir->SetGuidance("CRY initialization");
+  
+  FileCmd = new G4UIcmdWithAString("/CRY/file",this);
+  FileCmd->SetGuidance("This reads the CRY definition from a file");
+  FileCmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+   
+  InputCmd = new G4UIcmdWithAString("/CRY/input",this);
+  InputCmd->SetGuidance("CRY input lines");
+  InputCmd->AvailableForStates(G4State_PreInit,G4State_Idle);
+
+  UpdateCmd = new G4UIcmdWithoutParameter("/CRY/update",this);
+  UpdateCmd->SetGuidance("Update CRY definition.");
+  UpdateCmd->SetGuidance("This command MUST be applied before \"beamOn\" ");
+  UpdateCmd->SetGuidance("if you changed the CRY definition.");
+  UpdateCmd->AvailableForStates(G4State_Idle);
+  
+  cryDistanceZCmd = new G4UIcmdWithADoubleAndUnit("/CRY/distanceZ", this);
+  cryDistanceZCmd->SetGuidance("Set Z distance of the plane from which the cosmic rays are generated.");
+  cryDistanceZCmd->SetParameterName("cryDistanceZCmd", false);
+  cryDistanceZCmd->SetDefaultUnit("m");
+
+  MessInput = new std::string;
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PrimaryGeneratorMessenger::~PrimaryGeneratorMessenger()
+{
+  delete CRYDir;
+  delete InputCmd;
+  delete UpdateCmd;
+  delete FileCmd;
+  delete cryDistanceZCmd;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PrimaryGeneratorMessenger::SetNewValue(
+                                        G4UIcommand* command, G4String newValue)
+{ 
+  // if( command == InputCmd )
+  //  { 
+  //    Action->InputCRY();
+  //    (*MessInput).append(newValue);
+  //    (*MessInput).append(" ");
+  //  }
+
+  if( command == UpdateCmd )
+   { 
+     Action->UpdateCRY(MessInput); 
+     *MessInput = "";
+   }
+
+  if( command == FileCmd )
+   { Action->UpdateCRYFile(newValue); }
+
+  if (command == cryDistanceZCmd) {
+    Action->SetSourceZPosition(cryDistanceZCmd->GetNewDoubleValue(newValue));
+  }
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
